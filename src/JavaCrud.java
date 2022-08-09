99aebe31a68db061d3dcedddcc83978a861bbf3f
Juan Pablo Zaldumbide Proaño <jzaldumbide@gmail.com>
2022-08-09 11:41:45 -0500

    Initial commit

--- src/JavaCrud.java
+++ src/JavaCrud.java
@@ -14,6 +14,8 @@ public class JavaCrud {
     private JTextField textID;
     private JTextField textCantidad;
     private JButton limpiarButton;
+    private JButton buscarButton;
+    private JLabel textMensaje;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("JavaCrud");
@@ -52,6 +54,25 @@ public class JavaCrud {
                 Limpiar();
             }
         });
+
+        buscarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Buscar();
+            }
+        });
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Update();
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Delete();
+            }
+        });
     }
     Connection con;
     PreparedStatement pst;
@@ -79,7 +100,7 @@ public class JavaCrud {
         nombre=textNombre.getText();
         precio=textPrecio.getText();
         ciudad=textCiudad.getText();
-        //id= textNombre.getText();
+        id= textNombre.getText();
         cantidad=textCantidad.getText();
         System.out.println(nombre);
         System.out.println(precio);
@@ -113,7 +134,6 @@ public class JavaCrud {
 
         }
     }
-
     public void Limpiar(){
         textNombre.setText("");
         textPrecio.setText("");
@@ -121,5 +141,115 @@ public class JavaCrud {
         textID.setText("");
         textCantidad.setText("");
     }
+    public void Buscar(){
+        String id="0";
+        id=textID.getText();
 
+        final String DB_URL="jdbc:mysql://localhost/misproductos?serverTimezone=UTC";
+        final String USERNAME="jp";
+        final String PASSWORD="12345";
+
+
+        try{
+            Connection conn= DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            Statement stmt= conn.createStatement();
+            String sql="select * from productos where pid=?";
+            PreparedStatement pst=conn.prepareStatement(sql);
+            pst.setString(1,id);
+            //System.out.println(sql);
+
+
+            ResultSet rs=pst.executeQuery();
+
+
+            if(rs.next()==true){
+                String nombre, ciudad, precio, cantidad;
+                nombre=rs.getString(2);
+                ciudad=rs.getString(3);
+                precio=rs.getString(4);
+                cantidad=rs.getString(5);
+
+                System.out.println();
+                textNombre.setText(nombre);
+                textCiudad.setText(ciudad);
+                textPrecio.setText(precio);
+                textCantidad.setText(cantidad);
+
+            }
+            else {
+                //textMensaje.setText("no se encuentra el producto");
+                JOptionPane.showMessageDialog(null,"no se encuentra el producto");
+                Limpiar();
+            }
+            stmt.close();
+            conn.close();
+
+        } catch(SQLException ex){
+            ex.printStackTrace();
+            System.out.println("SQL incorrecto");
+
+        }
+    }
+    public void Update(){
+        String id, nombre, ciudad, precio, cantidad;
+        id=textID.getText();
+        nombre=textNombre.getText();
+        ciudad=textCiudad.getText();
+        precio=textPrecio.getText();
+        cantidad=textCantidad.getText();
+
+
+
+        final String DB_URL="jdbc:mysql://localhost/misproductos?serverTimezone=UTC";
+        final String USERNAME="jp";
+        final String PASSWORD="12345";
+
+
+        try{
+            Connection conn= DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            Statement stmt= conn.createStatement();
+            String sql="update productos set pnombre=?, pciudad=?,pprecio=?,pcantidad=? where pid=?";
+            PreparedStatement pst=conn.prepareStatement(sql);
+            pst.setString(1,nombre);
+            pst.setString(2,ciudad);
+            pst.setString(3,precio);
+            pst.setString(4,cantidad);
+            pst.setString(5,id);
+            //ResultSet resultSet=pst.executeQuery();
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null,"Registro actualizado");
+            stmt.close();
+            conn.close();
+
+        } catch(SQLException ex){
+            ex.printStackTrace();
+            System.out.println("SQL incorrecto");
+
+        }
+    }
+
+    public void Delete(){
+        final String DB_URL="jdbc:mysql://localhost/misproductos?serverTimezone=UTC";
+        final String USERNAME="jp";
+        final String PASSWORD="12345";
+        String borrarid=textID.getText();
+
+        try{
+            Connection conn= DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            Statement stmt= conn.createStatement();
+            String sql="delete from productos where pid=?";
+            PreparedStatement pst=conn.prepareStatement(sql);
+            pst.setString(1,borrarid);
+
+            pst.executeUpdate();
+            JOptionPane.showMessageDialog(null,"Registro borrado");
+            stmt.close();
+            conn.close();
+
+        } catch(SQLException ex){
+            ex.printStackTrace();
+            System.out.println("SQL incorrecto");
+
+        }
+    }
 }
